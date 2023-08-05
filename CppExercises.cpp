f605dca80b523184166b71797dafee407d509ade
NuminousBlue <jdiehl2236@gmail.com>
2023-08-04 21:03:46 -0500

    file io!

--- CppExercises.cpp
+++ CppExercises.cpp
@@ -4,9 +4,13 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <format>
 #include <chrono>
 #include <functional>
+
+using namespace std;
 
 int to_seconds(std::chrono::nanoseconds nanos) {
 	return nanos.count() / 1'000'000'000LL;
@@ -16,6 +20,8 @@ int to_millis(std::chrono::nanoseconds nanos) {
 	return nanos.count() / 1'000'000;
 }
 
+// in angle brackets goes the `template argument list`
+//   that is, any generics 
 template <typename Func>
 auto timer(Func func) {
 	return [func](auto&&... args) {
@@ -52,9 +58,42 @@ int fibonacci(int count) {
 	return second;
 }
 
+void write_file(const char* filepath, const char* content) {
+	std::cout << "Writing file to " << filepath << "\n";
+
+	std::ofstream ofile(filepath);
+
+	ofile << content << "\n";
+
+	ofile.close();
+}
+
+string read_file(const char* filepath) {
+	static char buf[128];
+
+	std::ifstream infile(filepath);
+
+	string content((istreambuf_iterator<char>(infile)),
+		(istreambuf_iterator<char>()));
+
+	infile.close();
+
+	return content;
+}
+
+
 int main() {
 
 	auto timed_fib = timer(fibonacci);
 
 	timed_fib(500);
+
+	const char* file_path = { "test_out.txt" };
+	const char* write_content = { "This is the test text content!" };
+
+	write_file(file_path, write_content);
+
+	string read_contents = read_file(file_path);
+
+	std::cout << "Read file contents:\n" << "::: " << read_contents;
 }
